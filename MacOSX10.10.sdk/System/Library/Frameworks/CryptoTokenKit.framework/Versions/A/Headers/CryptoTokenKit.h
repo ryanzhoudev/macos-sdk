6c7c1a3ff3c0cb36cc797e4a9189c5bad60be118
Ryan Zhou <57310593+ryanzhoudev@users.noreply.github.com>
2025-03-21 21:58:19 -0700

    Delete MacOSX10.10.sdk directory

--- MacOSX10.10.sdk/System/Library/Frameworks/CryptoTokenKit.framework/Versions/A/Headers/CryptoTokenKit.h
+++
@@ -1,8 +0,0 @@
-//
-//  CryptoTokenKit.h
-//  Copyright (c) 2014 Apple. All rights reserved.
-//
-
-#import <CryptoTokenKit/TKError.h>
-#import <CryptoTokenKit/TKSmartCard.h>
-#import <CryptoTokenKit/TKSmartCardATR.h>
